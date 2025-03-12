97716a1572cf3c6f6c51e61424e642e574bb46dd
shivansh9794 <mshivansh549@gmail.com>
2025-03-12 11:06:34 +0530

    added RTC

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,16 @@ const io = new Server(5000,{
     cors:true,
 });
 
+const emailToSocketIdMap = new Map();
+const socketIdToEmailMap = new Map();
+
 io.on("connection", socket=>{
     console.log("socket Connected with ",socket.id); 
     socket.on('room:join',data=>{
+        const {email,room}=data;
+        emailToSocketIdMap(email,socket.id);
+        socketIdToEmailMap(socket.id,email);
         console.log("socket's data --",data);
+        socket.to("room:join",data);
     })
 });
